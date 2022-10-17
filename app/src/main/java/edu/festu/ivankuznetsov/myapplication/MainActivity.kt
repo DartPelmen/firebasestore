b83631a3678b8ac53b7b71bd96e74f77d971b082
Ivan Kuznetcov <kyzaiv1@yandex.ru>
2022-10-17 16:13:19 +1000

    Initial commit

---
+++ app/src/main/java/edu/festu/ivankuznetsov/myapplication/MainActivity.kt
@@ -0,0 +1,75 @@
+package edu.festu.ivankuznetsov.myapplication
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        val database = Firebase.database("https://festu-sample-firebase-default-rtdb.europe-west1.firebasedatabase.app")
+//        val myRef = database.getReference("message")
+//
+////        myRef.setValue("Hello, World!")
+//
+//        myRef.addValueEventListener(object: ValueEventListener {
+//
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                val value = snapshot.value.toString()
+//                Log.d(TAG, "Value is: $value")
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                Log.w(TAG, "Failed to read value.", error.toException())
+//            }
+//
+//        })
+
+        val db = Firebase.firestore
+
+
+        val user = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912
+        )
+//
+//// Add a new document with a generated ID
+//        db.collection("users")
+//            .add(user)
+//            .addOnSuccessListener { documentReference ->
+//                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+//            }
+//            .addOnFailureListener { e ->
+//                Log.w(TAG, "Error adding document", e)
+//            }
+//    }
+
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+
+    }
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+}
